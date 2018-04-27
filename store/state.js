2612cda1843739c07e9f50041bfc419272570b0e
胡英军 <yingjun.hu@geely.com>
2018-04-27 13:10:40 +0800

    更改环境语言

--- store/state.js
+++ store/state.js
@@ -2,7 +2,7 @@ const browserLang =
         window.navigator.language || window.navigator.browserLanguage;
 const language =
         window.localStorage.getItem('userLang') ||
-        (browserLang.match(/\S*(?=-)/)[0] === 'en' ? 'en' : browserLang);
+        (browserLang.match(/\S*(?=-)/)[0] === 'zh' ? 'zh-CN' : 'en');
 
 export default {
     language,
