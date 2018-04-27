9f3333045bfe6569fd4e6a03eff39d118fdda054
Mcqun <mchqun@126.com>
2018-04-27 13:13:59 +0800

    依赖调整

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import Vue from 'vue';
 import sweetRouter from './router';
 import sweetStore from './store';
 import SWXHR from './xhr';
 import SWTOOL from './tool';
+import ElementUI from 'element-ui';
 
+Vue.use(ElementUI);
+Vue.use(SWTOOL);
+
+export { Vue };
 export { sweetRouter };
 export { sweetStore };
 export { SWXHR };
-export { SWTOOL };
