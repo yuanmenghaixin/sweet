1e3458a47561cd29fce448cc003d85ca7b89c0e5
Fengyuan Chen <fengyuan.chen@geely.com>
2019-01-18 15:44:47 +0800

    perf: simplify code

--- countly/index.js
+++ countly/index.js
@@ -3,35 +3,9 @@ import Countly from 'countly-sdk-web';
 
 /**
  * Countly初始化
- * @param Vue vue对象
- * @param options countly初始化时的对象
+ * @param options Countly初始化时的对象
  */
 export default function(options = {}) {
-    const {
-        app_key,
-        url,
-        device_id,
-        app_version,
-        country_code,
-        city,
-        ip_address,
-        debug,
-        ignore_bots,
-        interval,
-        fail_timeout
-    } = options
-    Countly.init({
-        app_key,
-        url,
-        device_id,
-        app_version,
-        country_code,
-        city,
-        ip_address,
-        debug,
-        ignore_bots,
-        interval,
-        fail_timeout
-    })
-    Vue.prototype.$Countly = Countly
+    Countly.init(options);
+    Vue.prototype.$Countly = Countly;
 }
